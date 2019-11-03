42177b2c8fb63a207e1fe1693be38c4c76380093
ablakerose <bdecktor@gmail.com>
2019-11-02 20:34:46 -0500

    edit flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,16 +9,19 @@ def destroy
 end
 
 def create
-    user = User.find_by(username: params[:user][:username])
-    if user && user.authenticate(params[:user][:password])
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
         #if we find the user and their password is correct...
-        session[:user_id] = user.id
-        redirect_to user_path(user) #"/users/#(user.id)"
+        session[:user_id] = @user.id
+        flash[:success] = "Welcome, #{@user.username}!"
+        redirect_to user_path(@user) #"/users/#(user.id)"
     else 
-        flash[:message] = "Incorrect log in information. Please try again."
-        redirect_to "/login"
+        flash[:message] = "Incorrect login. Please try again."
+        redirect_to login_path
     end
 end
 
+def new
+end
 
 end
