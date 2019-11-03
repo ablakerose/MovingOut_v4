28ae1c36adab5824c002b021f7250212c5c50fed
ablakerose <bdecktor@gmail.com>
2019-11-02 20:34:03 -0500

    add item_params helper method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,37 @@
 class ItemsController < ApplicationController
+
+    def new
+        @item= Item.new
+    end
+
+    def create
+        @item = current_user.items.build(item_params)
+        #number 1 reason something doesn't save because one of the
+        #belongs_to relationships is not fulfilled
+        if @item.save
+            redirect_to items_path
+        #LEFT OFF HERE. 
+        else
+            render :new
+        end
+        #generally, this is the logic for the create action. Usually doesn't need to be very long.
+    
+    def show
+        @item = Item.find_by_id(params[:id])
+    end
+
+    def index
+        @items = Item.all
+    end
+
+end
+
+private
+
+    def item_params
+        params.require(:item).permit(:list_price, :item_name, :sold)
+    end
+
+
+
 end
